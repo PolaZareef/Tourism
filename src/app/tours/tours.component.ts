610e1f51e5a0d903b8914ad89fcc04832da7b314
polazareef <polazareef65@gmail.com>
2022-09-25 17:07:25 +0200

    end of site coding

--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,Input} from '@angular/core';
+import {TourData} from './tours.interface';
 @Component({
   selector: 'app-tours',
   templateUrl: './tours.component.html',
   styleUrls: ['./tours.component.scss']
 })
 export class ToursComponent implements OnInit {
+  @Input()
+  tourData:TourData[]=[];
 
   constructor() { }
 
